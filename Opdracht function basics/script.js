213a229a3018e95228bd25d865b751b43f7012b8
Joost Kolb <joostkolb@hotmail.com>
2021-04-02 21:43:07 +0200

    Add files via upload

---
+++ Opdracht function basics/script.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log('Make dough');
+    console.log('Add sauce');
+    console.log('Add spices');
+    console.log('Add cheese');
+    console.log('Add Peperoni and Salami');
+}
+
+const makeSushi = function () {
+    console.log('Make paper');
+    console.log('make rice');
+    console.log('Add seaweed');
+    console.log('Add vegetables');
+    console.log('Roll the sushi');
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
